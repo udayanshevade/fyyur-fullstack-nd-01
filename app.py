f99553a31ec483b067adcba2f76bb091d3e24ecc
Udayan Shevade <udayan.shevade@gmail.com>
2021-07-05 13:42:05 -0700

    init Genre db, migration, update db

--- app.py
+++ app.py
@@ -29,6 +29,20 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+venue_genres = db.Table('venue_genres',
+                        db.Column('venue_id', db.Integer, db.ForeignKey(
+                            'Venue.id'), nullable=False),
+                        db.Column('genre_id', db.Integer, db.ForeignKey(
+                            'Genre.id'), nullable=False)
+                        )
+
+artist_genres = db.Table('artist_genres',
+                         db.Column('artist_id', db.Integer, db.ForeignKey(
+                             'Artist.id'), nullable=False),
+                         db.Column('genre_id', db.Integer, db.ForeignKey(
+                             'Genre.id'), nullable=False)
+                         )
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -49,6 +63,8 @@ class Venue(db.Model):
     website = db.Column(db.String(500), nullable=True)
 
     shows = db.relationship('Show', backref='venue')
+    genres = db.relationship(
+        'Genre', secondary=venue_genres, backref=db.backref('venue'))
 
 
 class Artist(db.Model):
@@ -61,8 +77,6 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
 
-    genres = db.Column(db.String(120))
-
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
@@ -71,6 +85,8 @@ class Artist(db.Model):
     website = db.Column(db.String(500), nullable=True)
 
     shows = db.relationship('Show', backref='artist')
+    genres = db.relationship(
+        'Genre', secondary=artist_genres, backref=db.backref('artist'))
 
 
 class Show(db.Model):
@@ -83,6 +99,13 @@ class Show(db.Model):
 
     end_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
     start_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
