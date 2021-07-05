6e7fe0bbe528c62c93725e3221ea836df19fa29a
Udayan Shevade <udayan.shevade@gmail.com>
2021-07-05 13:23:51 -0700

    add Show model, migration, upgrade db

--- app.py
+++ app.py
@@ -35,15 +35,20 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+
     seeking_description = db.Column(db.String, nullable=True)
     seeking_talent = db.Column(db.Boolean, default=False)
     website = db.Column(db.String(500), nullable=True)
+
+    shows = db.relationship('Show', backref='venue')
 
 
 class Artist(db.Model):
@@ -51,15 +56,33 @@ class Artist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+
     genres = db.Column(db.String(120))
+
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+
     seeking_description = db.Column(db.String, nullable=True)
     seeking_venue = db.Column(db.Boolean, default=False)
     website = db.Column(db.String(500), nullable=True)
+
+    shows = db.relationship('Show', backref='artist')
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+    end_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    start_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
