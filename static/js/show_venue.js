d60a387022cf88752e4ffaa93fdc1199a689f867
Udayan Shevade <udayan.shevade@gmail.com>
2021-07-07 01:12:57 -0700

    add delete button and functionality in show_venue page, include script

---
+++ static/js/show_venue.js
@@ -0,0 +1,21 @@
+(() => {
+  const handleDeleteVenue = async (e) => {
+    const id = e.target.dataset.venueId;
+    try {
+      const res = await fetch(`/venues/${id}`, { method: 'delete' });
+      const val = await res.json();
+      if (val) {
+        window.location.replace('/venues');
+      } else {
+        // TODO: replace with better error handling
+        alert('Failed to delete the venue. Try again later.');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    return false;
+  };
+
+  const btn = document.getElementById('btnDeleteVenue');
+  btn.addEventListener('click', handleDeleteVenue);
+})();
